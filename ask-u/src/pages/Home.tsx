e6900366158a8484c01051270a4f8280aec49729
govarthan v <231401030@rajalakshmi.edu.in>
2025-06-14 20:56:06 +0530

    Update Home.tsx

--- ask-u/src/pages/Home.tsx
+++ ask-u/src/pages/Home.tsx
@@ -1,6 +1,6 @@
 const Home = () => {
   const loginWithGoogle = () => {
-    window.location.href = "http://localhost:4000/api/auth/google";
+    window.location.href = "https://ask-u-like.onrender.com/api/auth/google";
   };
 
   return (
