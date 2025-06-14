7bea9a9cd062fb265e20803c9aa5bba7aa4bd981
govarthan v <231401030@rajalakshmi.edu.in>
2025-06-14 20:56:38 +0530

    Update Login.tsx

--- ask-u/src/pages/Login.tsx
+++ ask-u/src/pages/Login.tsx
@@ -1,7 +1,7 @@
 
 const Login = () => {
   const handleGoogleLogin = () => {
-    window.location.href = "http://localhost:4000/api/auth/google";
+    window.location.href = "https://ask-u-like.onrender.com/api/auth/google";
   };
 
   return (
